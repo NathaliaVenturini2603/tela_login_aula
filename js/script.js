1622fcae5c5a409fc435503ad0a4dabbbbc0f4c6
NathaliaVenturini2603 <nathalia.v.dias@aluno.senai.br>
2024-10-31 10:50:08 -0300

    tela login e registro

--- js/script.js
+++ js/script.js
@@ -1,34 +1,51 @@
 const form = document.getElementById('form');
+const name_input = document.getElementById('name-input');
 const email_input = document.getElementById('email-input');
 const password_input = document.getElementById('password-input');
+const repeat_password_input = document.getElementById('repeat-password-input');
 const error_message = document.getElementById('error-message');
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     let errors = [];
 
-    errors = validateLoginForm(
-        email_input.value,
-        password_input.value
-    );
-      // verificar login 
-      if (errors.length === 0){
-        const isValidLogin = verifyLogin( 
+    const filename = e.target.baseURI.split("/").pop();
+
+    if (filename === "register.html") {
+        errors = validateRegisterForm(
+            name_input.value,
             email_input.value,
-            password_input.value 
+            password_input.value,
+            repeat_password_input.value
         );
-        if (!isValidLogin){
-            errors.push('Email ou senha incorretos');
+    } else{
+        errors = validateLoginForm(
+            email_input.value,
+            password_input.value
+        );
+    
+        // Verificar Login
+        if (errors.length === 0) {
+            const isValidLogin = verifyLogin(
+                email_input.value,
+                password_input.value
+            );
+    
+            if (!isValidLogin) {
+                errors.push('Email ou senha incorretos');
+            }
         }
-        
-      }
-      
+    }
+    
+
     if (errors.length > 0) {
         error_message.innerText = errors.join(". ");
         return;
     }
+
     window.location.href = "home.html";
 });
+
 function validateLoginForm(email, password) {
     let errors = [];
 
@@ -36,17 +53,20 @@ function validateLoginForm(email, password) {
         errors.push('Email é obrigatório');
         email_input.parentElement.classList.add("incorrect");
     }
+
     if (password === "" || password == null) {
-        errors.push('Senha é obrigatório');
-        password_input.parentElement.classList.add("incorrect");
+        errors.push('Senha é obrigatória');
+        password_input.parentElement.classList.add("incorrect")
     }
 
     return errors;
 }
 
 const allInputs = [
+    name_input,
     email_input,
     password_input,
+    repeat_password_input
 ].filter((input) => input != null);
 
 allInputs.forEach((input) => {
@@ -81,3 +101,40 @@ function verifyLogin(email, password) {
         return false;
     }
 }
+
+function validateRegisterForm(name, email, password, repeatPassword) {
+    let errors = [];
+  
+    if (name === "" || name == null) {
+      errors.push("Nome é obrigatório");
+      name_input.parentElement.classList.add("incorrect");
+    }
+  
+    if (email === "" || email == null) {
+      errors.push("Email é obrigatório");
+      email_input.parentElement.classList.add("incorrect");
+    }
+  
+    if (password === "" || password == null) {
+      errors.push("Senha é obrigatória");
+      password_input.parentElement.classList.add("incorrect");
+    } else {
+      if (password.length < 8) {
+        errors.push("Senha deve ter no mínimo 8 caracteres");
+        password_input.parentElement.classList.add("incorrect");
+      }
+    }
+  
+    if (repeatPassword === "" || repeatPassword == null) {
+      errors.push("Repita a senha");
+      repeat_password_input.parentElement.classList.add("incorrect");
+    } else {
+      if (password !== repeatPassword) {
+        errors.push("Senhas não conferem");
+        password_input.parentElement.classList.add("incorrect");
+        repeat_password_input.parentElement.classList.add("incorrect");
+      }
+    }
+  
+    return errors;
+  }
